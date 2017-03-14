265c9dcb9b8e81b407a28d25d562d1b4ad0e6a9e
Jim Howes <das.howes@gmail.com>
2017-03-13 20:47:18 -0500

    First

---
+++ specs/searchSpec.js
@@ -0,0 +1,147 @@
+var SearchForm = function() {
+
+	// Get page elements
+	this.locationField = element(by.model("searchForm.params.location"));
+	this.programField = element(by.model("searchForm.params.program"));
+	this.dateField1 = element(by.model("searchForm.params.checkIn"));
+	this.dateField2 = element(by.model("searchForm.params.checkOut"));
+	this.guestsField = element(by.model("searchForm.params.adults"));
+	this.roomsField = element(by.model("searchForm.params.rooms"));
+	this.submitButton = element(by.css(".rm-btn-orange.search-submit-btn"));
+
+	// Access toggle/modal menus within page elements
+	this.locationMenu = element(by.css(".tt-dropdown-menu"));
+	this.programMenu = this.programField.element(by.css(".btn-group")).$$(".dropdown-menu");
+	this.datePicker = element(by.css(".ui-datepicker"));
+	this.guestsMenu = this.guestsField.element(by.css(".btn-group")).$$(".dropdown-menu");
+	this.roomsMenu = this.roomsField.element(by.css(".btn-group")).$$(".dropdown-menu");
+
+	// Access clickable elements within toggle/modal menus
+	this.locationChoice = this.locationMenu.all(by.css(".tt-dataset-placePredictions")).get(0);
+	this.programChoice = this.programMenu.all(by.tagName("li")).get(0);
+	this.dateChoice1 = this.datePicker.all(by.tagName("a")).get(0);
+	this.dateChoice2 = this.datePicker.all(by.tagName("a")).get(2);
+	this.guestsChoice = this.guestsMenu.all(by.tagName("a")).get(0);
+	this.roomsChoice = this.roomsMenu.all(by.tagName("li")).get(0);
+
+}
+
+// Checks to see if all of the pieces of the search form are present
+describe('search form display', function() {
+	var searchForm = new SearchForm();
+
+	beforeEach(function() {
+		browser.get('http://www.rocketmiles.com');
+		browser.ignoreSynchronization = true;
+		browser.waitForAngular();
+	});
+
+	it('should display the search form', function() {
+
+		// expect(searchForm.locationField.isPresent()).toBe(true);
+		//expect(homePage.locationField.isPresent()).toBe(false);
+
+		expect(searchForm.locationField.isDisplayed()).toBe(true);
+		//expect(homePage.locationField.isDisplayed()).toBe(false);
+
+		// expect(searchForm.programField.isPresent()).toBe(true);
+		//expect(homePage.programField.isPresent()).toBe(false);
+
+		expect(searchForm.programField.isDisplayed()).toBe(true);
+		//expect(homePage.programField.isDisplayed()).toBe(false);
+
+		// expect(searchForm.dateField1.isPresent()).toBe(true);
+		//expect(homePage.dateField1.isPresent()).toBe(false);
+
+		expect(searchForm.dateField1.isDisplayed()).toBe(true);
+		//expect(homePage.dateField1.isDisplayed()).toBe(false);
+
+		// expect(searchForm.dateField2.isPresent()).toBe(true);
+		//expect(homePage.dateField2.isPresent()).toBe(false);
+
+		expect(searchForm.dateField2.isDisplayed()).toBe(true);
+		//expect(homePage.dateField2.isDisplayed()).toBe(false);
+
+		// expect(searchForm.guestsField.isPresent()).toBe(true);
+		//expect(homePage.guestsField.isPresent()).toBe(false);
+
+		expect(searchForm.guestsField.isDisplayed()).toBe(true);
+		//expect(homePage.guestsField.isDisplayed()).toBe(false);
+
+		// expect(searchForm.roomsField.isPresent()).toBe(true);
+		//expect(homePage.roomsField.isPresent()).toBe(false);
+
+		expect(searchForm.roomsField.isDisplayed()).toBe(true);
+		//expect(homePage.roomsField.isDisplayed()).toBe(false);
+
+		// expect(searchForm.submitButton.isPresent()).toBe(true);
+		//expect(homePage.submitButton.isPresent()).toBe(false);
+
+		expect(searchForm.submitButton.isDisplayed()).toBe(true);
+		//expect(homePage.submitButton.isDisplayed()).toBe(false);
+
+		expect(searchForm.submitButton.getText()).toBe("Search Hotels and Earn Miles");
+	});
+});
+
+// Checks to see if all of the pieces of the search form are working properly
+describe('search form functionality', function() {
+	var searchForm = new SearchForm();
+
+	beforeEach(function() {
+		browser.get('http://www.rocketmiles.com');
+		browser.ignoreSynchronization = true;
+		browser.waitForAngular();
+	});
+
+	it('should enter a location', function() {
+		searchForm.locationField.click();
+		searchForm.locationField.sendKeys('Los Angeles, CA, United States');
+		browser.actions().sendKeys(protractor.Key.ENTER).perform()
+	})
+
+	// Check to see if program selection dropdown menu is working properly
+	it('should display the program list and pick a program', function() {
+
+		searchForm.programField.click();
+		browser.waitForAngular();
+		searchForm.programChoice.click();
+	});
+
+	// Check to see if the datepicker is working properly
+	it('should pick dates', function() {
+
+		searchForm.dateField1.click();
+		browser.waitForAngular();
+		searchForm.dateChoice1.click();
+		// Datepicker should shift over to the return date field
+		searchForm.dateChoice2.click();
+	});
+
+	// Check to see if the guests menu is working properly
+	it('should display the guests menu and pick a number of guests', function() {
+
+		searchForm.guestsField.click();
+		browser.waitForAngular();
+		searchForm.guestsChoice.click();
+	});
+
+	// Check to see if the rooms menu is working properly
+	it('should display the rooms menu and pick a number of rooms', function() {
+
+		searchForm.roomsField.click();
+		browser.waitForAngular();
+		searchForm.roomsChoice.click();
+	});
+
+	// Ensure form submission works properly
+	it('should click the submit button', function() {
+
+		searchForm.submitButton.click();
+	});
+});
+
+
+
+
+
